11e4202b6ac869a26c1dcbb760783e021a1e30ee
mast <mast@localhost>
2002-12-17 18:37:08 +0000

    Adding a declaration

--- imod/imod_display.h
+++ imod/imod_display.h
@@ -1,37 +1,18 @@
-/*  IMOD VERSION 2.7.8
+/*   imodv_display.h  -  declarations for imodv_display.cpp
  *
- *  $Id$
- *
- *  Original Author: David Mastronarde   email: mast@colorado.edu
- */
-
-/*****************************************************************************
- *   Copyright (C) 1995-1998 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
+ *   Copyright (C) 1995-2002 by Boulder Laboratory for 3-Dimensional Electron
+ *   Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *   Colorado.  See implementation file for full copyright notice.
+ */                                                                           
 /*  $Author$
 
 $Date$
 
 $Log$
-*/
+Revision 1.1.2.1  2002/12/14 05:46:23  mast
+Initial creation
 
+*/
 
 #ifndef IMOD_DISPLAY_H
 #define IMOD_DISPLAY_H
@@ -59,6 +40,7 @@ extern "C" {
 #endif
   void imodAssessVisuals();
   void imodFindQGLFormat(ImodApp *ap, char **argv);
+  ImodGLVisual *imodFindGLVisual(ImodGLRequest request);
 
 #ifdef __cplusplus
 }
