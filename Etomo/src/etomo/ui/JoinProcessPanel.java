5b3a1f03fb87625b867a270b890aa0310720e874
sueh <sueh@localhost>
2004-09-08 20:06:09 +0000

    bug# 520 AxisProcessPanel for Join

---
+++ Etomo/src/etomo/ui/JoinProcessPanel.java
@@ -0,0 +1,28 @@
+package etomo.ui;
+
+import etomo.JoinManager;
+import etomo.type.AxisID;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2002, 2003, 2004</p>
+*
+*<p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+
+
+public class JoinProcessPanel extends AxisProcessPanel {
+  public static  final String  rcsid =  "$Id$";
+  
+  public JoinProcessPanel(JoinManager joinManager, AxisID axis) {
+    super(joinManager, axis);
+  }
+}
