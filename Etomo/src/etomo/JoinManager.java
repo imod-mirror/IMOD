5c48483d17ba08a0f4d8b545bb9b61ae3aa4ca5d
sueh <sueh@localhost>
2004-09-15 22:34:34 +0000

    bug# 520 casting  base manager when necessary.  Added JoinDialog

--- Etomo/src/etomo/JoinManager.java
+++ Etomo/src/etomo/JoinManager.java
@@ -2,6 +2,11 @@ package etomo;
 
 import java.io.File;
 
+import etomo.type.AxisID;
+import etomo.type.AxisType;
+import etomo.ui.JoinDialog;
+import etomo.ui.MainJoinPanel;
+
 /**
 * <p>Description: </p>
 * 
@@ -16,6 +21,9 @@ import java.io.File;
 * @version $Revision$
 * 
 * <p> $Log$
+* <p> Revision 1.1.2.4  2004/09/13 16:41:18  sueh
+* <p> bug# 520 added isNewManager stub function
+* <p>
 * <p> Revision 1.1.2.3  2004/09/08 19:28:25  sueh
 * <p> bug# 520 update call to BaseMAnager()
 * <p>
@@ -29,12 +37,20 @@ import java.io.File;
 public class JoinManager extends BaseManager {
   public static  final String  rcsid =  "$Id$";
   
+  //  Process dialog references
+  private JoinDialog joinDialog = null;
+  
+  //convenience variable set equals to mainPanel
+  //use through castMainPanel
+  private MainJoinPanel mainJoinPanel = null; 
+  
   public JoinManager(String paramFileName) {
     super();
+    openJoinDialog();
   }
   
   public boolean isNewManager() {
-    return false;
+    return true;
   }
 
   protected void createComScriptManager() {
@@ -53,7 +69,51 @@ public class JoinManager extends BaseManager {
     
   }
   
-  protected void createMainPanel() {
+  /**
+   * Open the join dialog
+   */
+  public void openJoinDialog() {
+    openProcessingPanel();
+    if (joinDialog == null) {
+      joinDialog = new JoinDialog(this);
+    }
+    mainPanel.showProcess(joinDialog.getContainer(), AxisID.ONLY);
   }
   
+  /**
+   *  
+   */
+  public void doneJoinDialog(AxisID axisID) {
+    if (joinDialog == null) {
+      mainPanel.openMessageDialog(
+        "Can not update join without an active join dialog",
+        "Program logic error");
+      return;
+    }
+    isDataParamDirty = true;
+    joinDialog = null;
+  }
+
+  
+  protected void createMainPanel() {
+    mainPanel = new MainJoinPanel(this);
+  }
+  
+  private MainJoinPanel castMainPanel() {
+    if (mainPanel == null) {
+      throw new NullPointerException();
+    }
+    if (mainJoinPanel == null) {
+      mainJoinPanel = (MainJoinPanel) mainPanel;
+    }
+    return mainJoinPanel;
+  }
+  
+  /**
+   * Open the main window in processing mode
+   */
+  private void openProcessingPanel() {
+    mainPanel.showProcessingPanel(AxisType.SINGLE_AXIS);
+    setPanel();
+  }
 }
