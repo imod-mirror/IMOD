b895776280d1a85a86ffaa641e33e863acddc46a
sueh <sueh@localhost>
2004-09-15 22:42:21 +0000

    bug# 520 added castManger(), overrode createAxisPanelA and B

--- Etomo/src/etomo/ui/MainJoinPanel.java
+++ Etomo/src/etomo/ui/MainJoinPanel.java
@@ -1,6 +1,10 @@
 package etomo.ui;
 
+import javax.swing.JPanel;
+
+import etomo.EtomoDirector;
 import etomo.JoinManager;
+import etomo.type.AxisID;
 /**
 * <p>Description: </p>
 * 
@@ -14,19 +18,51 @@ import etomo.JoinManager;
 * 
 * @version $Revision$
 * 
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1.2.1  2004/09/08 20:09:02  sueh
+* <p> bug# 520 MainPanel for Join
+* <p> </p>
 */
 
 public class MainJoinPanel extends MainPanel {
   public static  final String  rcsid =  "$Id$";
   
-  JoinManager joinManager = null;
-  JoinProcessPanel axisPanelA = null;
+  //convenience variables set to super class member variables
+  //use through cast functions
+  private JoinManager joinManager = null;
+
   /**
    * @param joinManager
    */
   public MainJoinPanel(JoinManager joinManager) {
     super(joinManager);
   }
+  
+  protected void createAxisPanelA(AxisID axisID) {
+    axisPanelA = new JoinProcessPanel((JoinManager) manager, axisID);
+  }
+
+  protected void createAxisPanelB() {
+  }
+  
+  private JoinManager castManager() {
+    if (manager == null) {
+      throw new NullPointerException();
+    }
+    if (joinManager == null) {
+      joinManager = (JoinManager) manager;
+    }
+    return joinManager;
+  }
+  
+  /**
+   * Open the setup panel
+   */
+  public void openPanel(JPanel panel) {
+    scrollA.add(panel);
+    revalidate();
+    EtomoDirector.getMainFrame().pack();
+  }
+
 
 }
