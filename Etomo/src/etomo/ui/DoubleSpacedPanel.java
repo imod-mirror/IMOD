de72582cd5988d4f54e4f337b6a4119631157f0a
sueh <sueh@localhost>
2004-09-23 23:52:23 +0000

    bug# 520 Added a class description.

--- Etomo/src/etomo/ui/DoubleSpacedPanel.java
+++ Etomo/src/etomo/ui/DoubleSpacedPanel.java
@@ -8,7 +8,12 @@ import javax.swing.BoxLayout;
 import javax.swing.border.Border;
 
 /**
-* <p>Description: </p>
+* <p>Description: A box layout JPanel-like object that can either use X_AXIS or 
+* Y_AXIS.  It places rigid areas outside the components on both the x axis and 
+* the y axis.  It also places rigid areas between components.  It can be created
+* with or without a border.  It is most useful for bordered panels, and some
+* panels containing bordered panels.  It should work like a JPanel, except that
+* layout and border are set during construction.</p>
 * 
 * <p>Copyright: Copyright (c) 2002, 2003, 2004</p>
 *
@@ -20,7 +25,11 @@ import javax.swing.border.Border;
 * 
 * @version $Revision$
 * 
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1.2.1  2004/09/23 23:36:12  sueh
+* <p> bug# 520 A panel which has rigid areas on both the X axis and Y axis.
+* <p> Useful with borders and panels containing panels with borders.
+* <p> </p>
 */
 public class DoubleSpacedPanel {
   public static final String rcsid = "$Id$";
@@ -30,12 +39,13 @@ public class DoubleSpacedPanel {
   SpacedPanel innerPanel;
   SpacedPanel outerPanel;
 
-  public DoubleSpacedPanel(boolean xAxisLayout, Dimension xPixels, Dimension yPixels) {
+  public DoubleSpacedPanel(boolean xAxisLayout, Dimension xPixels,
+      Dimension yPixels) {
     this(xAxisLayout, xPixels, yPixels, null);
   }
 
-  public DoubleSpacedPanel(boolean xAxisLayout, Dimension xPixels, Dimension yPixels,
-      Border border) {
+  public DoubleSpacedPanel(boolean xAxisLayout, Dimension xPixels,
+      Dimension yPixels, Border border) {
     if (xAxisLayout) {
       outerPixels = yPixels;
       innerPixels = xPixels;
@@ -62,15 +72,15 @@ public class DoubleSpacedPanel {
   public Component add(Component comp) {
     return innerPanel.add(comp);
   }
-  
+
   public Component add(DoubleSpacedPanel doubleSpacedPanel) {
     return innerPanel.add(doubleSpacedPanel.getContainer());
   }
-  
+
   public Component add(SpacedPanel spacedPanel) {
     return innerPanel.add(spacedPanel.getContainer());
   }
-  
+
   public Container getContainer() {
     return outerPanel.getContainer();
   }
