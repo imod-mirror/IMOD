a62473b189a26415c201ed05c90efc7ffe483224
sueh <sueh@localhost>
2004-09-29 19:35:59 +0000

    bug# 520 Created private variables that are cast from base-class member variables during construction.

--- Etomo/src/etomo/ui/MainJoinPanel.java
+++ Etomo/src/etomo/ui/MainJoinPanel.java
@@ -1,10 +1,14 @@
 package etomo.ui;
 
+import java.io.File;
+
 import javax.swing.JPanel;
 
 import etomo.EtomoDirector;
 import etomo.JoinManager;
 import etomo.type.AxisID;
+import etomo.type.JoinMetaData;
+
 /**
 * <p>Description: </p>
 * 
@@ -19,6 +23,9 @@ import etomo.type.AxisID;
 * @version $Revision$
 * 
 * <p> $Log$
+* <p> Revision 1.1.2.2  2004/09/15 22:42:21  sueh
+* <p> bug# 520 added castManger(), overrode createAxisPanelA and B
+* <p>
 * <p> Revision 1.1.2.1  2004/09/08 20:09:02  sueh
 * <p> bug# 520 MainPanel for Join
 * <p> </p>
@@ -27,10 +34,6 @@ import etomo.type.AxisID;
 public class MainJoinPanel extends MainPanel {
   public static  final String  rcsid =  "$Id$";
   
-  //convenience variables set to super class member variables
-  //use through cast functions
-  private JoinManager joinManager = null;
-
   /**
    * @param joinManager
    */
@@ -45,16 +48,6 @@ public class MainJoinPanel extends MainPanel {
   protected void createAxisPanelB() {
   }
   
-  private JoinManager castManager() {
-    if (manager == null) {
-      throw new NullPointerException();
-    }
-    if (joinManager == null) {
-      joinManager = (JoinManager) manager;
-    }
-    return joinManager;
-  }
-  
   /**
    * Open the setup panel
    */
@@ -64,5 +57,24 @@ public class MainJoinPanel extends MainPanel {
     EtomoDirector.getMainFrame().pack();
   }
 
-
+  /**
+   * Set the status bar with the file name of the data parameter file
+   */
+  public void updateDataParameters(File paramFile, JoinMetaData joinMetaData) {
+    StringBuffer buffer = new StringBuffer();
+    if (joinMetaData == null) {
+      buffer.append("No data set loaded");
+    }
+    else {
+      if (paramFile == null) {
+        buffer.append("Data file: NOT SAVED");
+      }
+      else {
+        buffer.append("Data file: " + paramFile.getAbsolutePath());
+      }
+      buffer.append("   Axis type: ");
+      buffer.append(joinMetaData.getAxisType().toString());
+    }
+    statusBar.setText(buffer.toString());
+  }
 }
