dd2266f12931fcb43c2f8aa8f2d5f718db48b88f
sueh <sueh@localhost>
2004-09-29 19:28:47 +0000

    bug# 520 process tracking for serial sections.  Doesn't do anything yet.

---
+++ Etomo/src/etomo/type/JoinProcessTrack.java
@@ -0,0 +1,30 @@
+package etomo.type;
+
+import java.util.Properties;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2002, 2003, 2004</p>
+*
+*<p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public class JoinProcessTrack extends BaseProcessTrack {
+  public static  final String  rcsid =  "$Id$";
+  
+  public void load(Properties props, String prepend) {
+    
+  }
+  
+  public void store(Properties props, String prepend) {
+    
+  }
+}
