fe0c9655a18e47da205e13a29502d3805fd140bb
sueh <sueh@localhost>
2004-10-06 01:57:57 +0000

    bug# 520 Added Z max from header.  Added descriptions of the fields for invalidReason.  Added isValidForMakeSamples() - validate when Make Samples is pressed.

--- Etomo/src/etomo/type/ConstSectionTableRowData.java
+++ Etomo/src/etomo/type/ConstSectionTableRowData.java
@@ -18,7 +18,12 @@ import etomo.storage.Storable;
 * 
 * @version $Revision$
 * 
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1.2.1  2004/09/29 19:26:26  sueh
+* <p> bug# 520 Divided the SectionTable row into document and view.  This
+* <p> class is the const part of the document.  It implements Storable with
+* <p> abstract load functions, has an equals function and get functions.
+* <p> </p>
 */
 public abstract class ConstSectionTableRowData implements Storable {
   public static  final String  rcsid =  "$Id$";
@@ -34,6 +39,17 @@ public abstract class ConstSectionTableRowData implements Storable {
   protected static final String rotationAngleXString = "RotationAngleX";
   protected static final String rotationAngleYString = "RotationAngleY";
   protected static final String rotationAngleZString = "RotationAngleZ";
+  protected static final String zMaxString = "ZMax";
+  
+  protected static final String sampleBottomStartName = "Starting bottom sample slice";
+  protected static final String sampleBottomEndName = "Ending bottom sample slice";
+  protected static final String sampleTopStartName = "Starting top sample slice";
+  protected static final String sampleTopEndName = "Ending top sample slice";
+  protected static final String finalStartName = "Final starting slice";
+  protected static final String finalEndName = "Final ending slice";
+  protected static final String rotationAngleXName = "X rotation angle";
+  protected static final String rotationAngleYName = "Y rotation angle";
+  protected static final String rotationAngleZName = "Z rotation angle";
   
   protected int rowNumber;
   protected File section;
@@ -46,9 +62,60 @@ public abstract class ConstSectionTableRowData implements Storable {
   protected double rotationAngleX;
   protected double rotationAngleY;
   protected double rotationAngleZ;
+  protected int zMax = Integer.MIN_VALUE;
+  
+  protected StringBuffer invalidReason = null;
   
   public abstract void load(Properties props);
   public abstract void load(Properties props, String prepend);
+  
+  public boolean isValidForMakeSamples(int tableSize) {
+    invalidReason = new StringBuffer("Row " + rowNumber + ":  ");
+    if (section == null) {
+      invalidReason.append("The section has not been set.");
+      return false;
+    }
+    if (!section.exists()) {
+      invalidReason.append("The section, " + section.getAbsolutePath() + ", does not exist.");
+      return false;
+    }
+    if (!section.isFile()) {
+      invalidReason.append("The section, " + section.getAbsolutePath() + ", is not a file.");
+      return false;
+    }
+    if (!section.canRead()) {
+      invalidReason.append("The section, " + section.getAbsolutePath() + ", is not readable.");
+      return false;
+    }
+    if (rowNumber > 1 && !isValidSlice(sampleBottomStart, sampleBottomStartName)) {
+      return false;
+    }
+    if (rowNumber > 1 && !isValidSlice(sampleBottomEnd, sampleBottomEndName)) {
+      return false;
+    }
+    if (rowNumber < tableSize && !isValidSlice(sampleTopStart, sampleTopStartName)) {
+      return false;
+    }
+    if (rowNumber < tableSize && !isValidSlice(sampleTopEnd, sampleTopEndName)) {
+      return false;
+    }
+    invalidReason = null;
+    return true;
+  }
+  
+  private boolean isValidSlice(int slice, String description) {
+    if (slice == Integer.MIN_VALUE) {
+      invalidReason.append(description + " is required.");
+      return false;
+    }
+    if (slice < 1) {
+      invalidReason.append(description + " must be greater then 0");
+    }
+    if (zMax != Integer.MIN_VALUE && slice > zMax) {
+      invalidReason.append(description + " cannot be greater then " + zMax);
+    }
+    return true;
+  }
   
   public void store(Properties props) {
     store(props, "");
@@ -59,6 +126,7 @@ public abstract class ConstSectionTableRowData implements Storable {
     String group = prepend + ".";
     
     props.setProperty(group + rowNumberString, Integer.toString(rowNumber));
+    props.setProperty(group + zMaxString, Double.toString(zMax));
     props.setProperty(group + sectionString, section.getAbsolutePath());   
     props.setProperty(group + sampleBottomStartString, Integer.toString(sampleBottomStart));
     props.setProperty(group + sampleBottomEndString, Integer.toString(sampleBottomEnd));
@@ -135,6 +203,10 @@ public abstract class ConstSectionTableRowData implements Storable {
       return "";
     }
     return Double.toString(value);
+  }
+  
+  public String getInvalidReason() {
+    return invalidReason.toString();
   }
   
   public int getRowNumber() {
