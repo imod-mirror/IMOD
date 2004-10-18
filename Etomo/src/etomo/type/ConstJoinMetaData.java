2d9ec1f3ed31ed9e209b943b5d1f155a200e89e0
sueh <sueh@localhost>
2004-10-18 18:01:46 +0000

    bug# 520 Added fields from JoinDialog.  Converted densityRefSection to an EtomoInteger.  Added validation checks for rootName and workingDir.

--- Etomo/src/etomo/type/ConstJoinMetaData.java
+++ Etomo/src/etomo/type/ConstJoinMetaData.java
@@ -18,6 +18,9 @@ import java.util.Properties;
 * @version $Revision$
 * 
 * <p> $Log$
+* <p> Revision 1.1.2.6  2004/10/15 00:17:15  sueh
+* <p> bug# 520 Added toString().  Fixed createPrepend().
+* <p>
 * <p> Revision 1.1.2.5  2004/10/14 02:28:12  sueh
 * <p> bug# 520 Added getWorkingDir().
 * <p>
@@ -50,22 +53,44 @@ public abstract class ConstJoinMetaData extends BaseMetaData {
 
   protected static final String groupString = "Join";
   protected static final String sectionTableDataSizeString = "SectionTableDataSize";
-  protected static final String densityRefSectionString = "DensityRefSection";
   protected static final String workingDirString = "WorkingDir";
   protected static final String rootNameString = "RootName";
+  protected static final String fullLinearTransformationString = "FullLinearTransformation";
+  protected static final String rotationTranslationMagnificationString = "RotationTranslationMagnification";
+  protected static final String rotationTranslationString = "RotationTranslation";
+  protected static final String useAlignmentRefSectionString = "UseAlignmentRefSection";
 
-  protected static final int defaultDensityRefSection = 1;
+  protected static final boolean defaultFullLinearTransformation = true;
 
   protected ArrayList sectionTableData;
-  protected int densityRefSection;
   protected String workingDir;
   protected String rootName;
+  protected EtomoInteger densityRefSection = new EtomoInteger("DensityRefSection");;
+  protected EtomoDouble sigmaLowFrequency = new EtomoDouble("SigmaLowFrequency");
+  protected EtomoDouble cutoffHighFrequency = new EtomoDouble("CutoffHighFrequency");
+  protected EtomoDouble sigmaHighFrequency = new EtomoDouble("SigmaHighFrequency");
+  protected boolean fullLinearTransformation;
+  protected boolean rotationTranslationMagnification;
+  protected boolean rotationTranslation;
+  protected boolean useAlignmentRefSection;
+  protected EtomoInteger alignmentRefSection = new EtomoInteger("AlignmentRefSection");
+  protected EtomoInteger sizeInX = new EtomoInteger("SizeInX");
+  protected EtomoInteger sizeInY = new EtomoInteger("SizeInY");
+  protected EtomoFloat offsetInX = new EtomoFloat("OffsetInX");
+  protected EtomoFloat offsetInY = new EtomoFloat("OffsetInY");
 
   public abstract void load(Properties props);
   public abstract void load(Properties props, String prepend);
 
   public ConstJoinMetaData() {
     fileExtension = "ejf";
+    densityRefSection.setDefaultValue(1);
+    sigmaLowFrequency.setDefaultValue(0);
+    cutoffHighFrequency.setDefaultValue(0);
+    cutoffHighFrequency.setRecommendedValue(0.25);
+    sigmaHighFrequency.setDefaultValue(0);
+    sigmaHighFrequency.setRecommendedValue(0.05);
+    alignmentRefSection.setDefaultValue(1);
   }
   
   public String toString() {
@@ -73,15 +98,32 @@ public abstract class ConstJoinMetaData extends BaseMetaData {
   }
 
   protected String paramString() {
-    StringBuffer buffer = new StringBuffer(super.paramString() + ",\nlatestRevisionNumber=" + latestRevisionNumber + ",\nnewJoinTitle="
-        + newJoinTitle + ",\ngroupString=" + groupString + ",\n" + densityRefSectionString + "="
-        + densityRefSection + ",\n" + workingDirString + "="
-        + workingDir + ",\n" + rootNameString + "="
-        + rootName);
+    StringBuffer buffer = new StringBuffer(super.paramString()
+        + ",\nlatestRevisionNumber=" + latestRevisionNumber
+        + ",\nnewJoinTitle=" + newJoinTitle + ",\ngroupString=" + groupString
+        + ",\n" + densityRefSection.getDescription() + "=" + densityRefSection
+        + ",\n" + workingDirString + "=" + workingDir + ",\n" + rootNameString
+        + "=" + rootName + ",\n" + sigmaLowFrequency.getDescription() + "="
+        + sigmaLowFrequency + ",\n" + cutoffHighFrequency.getDescription()
+        + "=" + cutoffHighFrequency + ",\n"
+        + sigmaHighFrequency.getDescription() + "=" + sigmaHighFrequency
+        + ",\n" + fullLinearTransformationString + "="
+        + fullLinearTransformation + ",\n"
+        + rotationTranslationMagnificationString + "="
+        + rotationTranslationMagnification + ",\n" + rotationTranslationString
+        + "=" + rotationTranslation + ",\n" + useAlignmentRefSectionString
+        + "=" + useAlignmentRefSection + ",\n"
+        + alignmentRefSection.getDescription() + "=" + alignmentRefSection
+        + ",\n" + sizeInX.getDescription() + "=" + sizeInX + ",\n"
+        + sizeInY.getDescription() + "=" + sizeInY + ",\n"
+        + offsetInX.getDescription() + "=" + offsetInX + ",\n"
+        + offsetInY.getDescription() + "=" + offsetInY);
     if (sectionTableData != null) {
-      buffer.append(",\n"+ sectionTableDataSizeString + "=" + sectionTableData.size());
+      buffer.append(",\n" + sectionTableDataSizeString + "="
+          + sectionTableData.size());
       for (int i = 0; i < sectionTableData.size(); i++) {
-        ConstSectionTableRowData row = (ConstSectionTableRowData) sectionTableData.get(i);
+        ConstSectionTableRowData row = (ConstSectionTableRowData) sectionTableData
+            .get(i);
         buffer.append(row.toString());
       }
     }
@@ -99,10 +141,21 @@ public abstract class ConstJoinMetaData extends BaseMetaData {
     props.setProperty(group + revisionNumberString, latestRevisionNumber);
     props.setProperty(group + workingDirString, workingDir);
     props.setProperty(group + rootNameString, rootName);
-    props.setProperty(group + densityRefSectionString, Integer
-        .toString(densityRefSection));
+    densityRefSection.store(props, prepend);
     props.setProperty(group + sectionTableDataSizeString, Integer
         .toString(sectionTableData.size()));
+    sigmaLowFrequency.store(props, prepend);
+    cutoffHighFrequency.store(props, prepend);
+    sigmaHighFrequency.store(props, prepend);
+    props.setProperty(group + fullLinearTransformationString, Boolean.toString(fullLinearTransformation));
+    props.setProperty(group + rotationTranslationMagnificationString, Boolean.toString(rotationTranslationMagnification));
+    props.setProperty(group + rotationTranslationString, Boolean.toString(rotationTranslation));
+    props.setProperty(group + useAlignmentRefSectionString, Boolean.toString(useAlignmentRefSection));
+    alignmentRefSection.store(props, prepend);
+    sizeInX.store(props, prepend);
+    sizeInY.store(props, prepend);
+    offsetInX.store(props, prepend);
+    offsetInY.store(props, prepend);
     if (sectionTableData != null) {
       for (int i = 0; i < sectionTableData.size(); i++) {
         ((SectionTableRowData) sectionTableData.get(i)).store(props, prepend);
@@ -118,49 +171,43 @@ public abstract class ConstJoinMetaData extends BaseMetaData {
   }
 
   public boolean isValid() {
-    return true;
+    return isValid(false);
   }
 
   public boolean isValid(boolean fromScreen) {
-    return true;
-  }
-  
-  public boolean isValid(File paramFile) {
-    return true;
-  }
-
-  public boolean isValidForMakeSamples() {
-    if (workingDir == null || !workingDir.matches("\\S+")) {
-      invalidReason = "The working directory is not set";
+    if (fromScreen && (workingDir == null || !workingDir.matches("\\S+"))) {
+      invalidReason = workingDirString + " is empty.";
       return false;
     }
     File dir = new File(workingDir);
-    if (dir == null || !dir.exists()) {
-      invalidReason = "The directory," + workingDir + ", does not exist.";
-      return false;
-    }
     if (!dir.isDirectory()) {
-      invalidReason = "The working directory," + workingDir
-          + ", must be a directory.";
+      invalidReason = dir.getAbsolutePath() + ", must be a directory.";
       return false;
     }
-    if (!dir.canWrite()) {
-      invalidReason = "The working directory," + workingDir
-          + ", must be writable.";
+    if (!isValid(dir)) {
       return false;
     }
-    if (rootName == null || !rootName.matches("\\S+")) {
-      invalidReason = "The root name is not set.";
+    if (fromScreen && (rootName == null || !rootName.matches("\\S+"))) {
+      invalidReason = rootNameString + " is empty.";
       return false;
     }
-    int sectionTableDataSize = sectionTableData.size();
-    for (int i = 0; i < sectionTableDataSize; i++) {
-      SectionTableRowData row = (SectionTableRowData) sectionTableData.get(i);
-      if (!row.isValidForMakeSamples(sectionTableDataSize)) {
-        invalidReason = row.getInvalidReason();
-      }
+    return true;
+  }
+  
+  public boolean isValid(File file) {
+    if (file == null || !file.exists()) {
+      invalidReason = file.getAbsolutePath() + ", does not exist.";
+      return false;
     }
 
+    if (!file.canRead()) {
+      invalidReason = file.getAbsolutePath() + ", must be readable.";
+      return false;
+    }
+    if (!file.canWrite()) {
+      invalidReason = file.getAbsolutePath() + ", must be writable.";
+      return false;
+    }
     return true;
   }
 
@@ -194,7 +241,15 @@ public abstract class ConstJoinMetaData extends BaseMetaData {
     return rootName;
   }
   
+  public boolean isRootNameSet() {
+    return rootName != null && rootName.matches("\\S+");
+  }
+  
   public int getDensityRefSection() {
+    return densityRefSection.get();
+  }
+  
+  public ConstEtomoInteger getDensityRefSectionField() {
     return densityRefSection;
   }
 
@@ -215,8 +270,72 @@ public abstract class ConstJoinMetaData extends BaseMetaData {
   public String getWorkingDir() {
     return workingDir;
   }
+  
+  public boolean isWorkingDirSet() {
+    return workingDir != null && workingDir.matches("\\S+");
+  }
+  
+  public String getSigmaLowFrequencyString() {
+    return sigmaLowFrequency.toString();
+  }
+  
+  public ConstEtomoDouble getSigmaLowFrequencyField() {
+    return sigmaLowFrequency;
+  }
+  
+  public String getCutoffHighFrequencyString() {
+    return cutoffHighFrequency.toString();
+  }
+  
+  public ConstEtomoDouble getCutoffHighFrequencyField() {
+    return cutoffHighFrequency;
+  }
+  
+  public String getSigmaHighFrequencyString() {
+    return sigmaHighFrequency.toString();
+  }
+  
+  public ConstEtomoDouble getSigmaHighFrequencyField() {
+    return sigmaHighFrequency;
+  }
 
   public static String getNewFileTitle() {
     return newJoinTitle;
+  }
+  
+  public boolean isFullLinearTransformation() {
+    return fullLinearTransformation;
+  }
+  
+  public boolean isRotationTranslationMagnification() {
+    return rotationTranslationMagnification;
+  }
+  
+  public boolean isRotationTranslation() {
+    return rotationTranslation;
+  }
+  
+  public boolean isUseAlignmentRefSection() {
+    return useAlignmentRefSection;
+  }
+  
+  public int getAlignmentRefSection() {
+    return alignmentRefSection.get();
+  }
+  
+  public String getSizeInXString() {
+    return sizeInX.toString();
+  }
+  
+  public String getSizeInYString() {
+    return sizeInY.toString();
+  }
+  
+  public String getOffsetInXString() {
+    return offsetInX.toString();
+  }
+  
+  public String getOffsetInYString() {
+    return offsetInY.toString();
   }
 }
