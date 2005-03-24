f12bb8157f3f12efd79445989c308a1cfc94a8eb
sueh <sueh@localhost>
2005-03-24 19:34:19 +0000

    bug# 621 Moved the clean up panel in post processing to a separate dialog.

--- Etomo/src/etomo/ui/CleanUpDialog.java
+++ Etomo/src/etomo/ui/CleanUpDialog.java
@@ -1,15 +1,4 @@
 package etomo.ui;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-
-import etomo.ApplicationManager;
-import etomo.type.AxisID;
-import etomo.type.DialogType;
-
 /**
 * <p>Description: </p>
 * 
@@ -22,67 +11,9 @@ import etomo.type.DialogType;
 * @author $Author$
 * 
 * @version $Revision$
+* 
+* <p> $Log$ </p>
 */
-public class CleanUpDialog extends ProcessDialog implements ContextMenu {
+public class CleanUpDialog {
   public static  final String  rcsid =  "$Id$";
-  
-  private CleanupPanel cleanupPanel;
-  
-  public CleanUpDialog(ApplicationManager appMgr) {
-    super(appMgr, AxisID.ONLY, DialogType.CLEAN_UP);
-    rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.Y_AXIS));
-    cleanupPanel = new CleanupPanel(applicationManager);
-    rootPanel.add(cleanupPanel.getContainer());
-    rootPanel.add(Box.createVerticalGlue());
-    rootPanel.add(Box.createRigidArea(FixedDim.x0_y10));
-    rootPanel.add(pnlExitButtons);
-    rootPanel.add(Box.createRigidArea(FixedDim.x0_y10));
-    
-    btnAdvanced.setVisible(false);
-    btnExecute.setText("Done");
-
-
-    //  Mouse adapter for context menu
-    GenericMouseAdapter mouseAdapter = new GenericMouseAdapter(this);
-    rootPanel.addMouseListener(mouseAdapter);
-
-    // Set the default advanced dialog state
-    updateAdvanced();
-  }
-
-  /**
-   * Right mouse button context menu
-   */
-  public void popUpContextMenu(MouseEvent mouseEvent) {
-    new ContextPopup(rootPanel, mouseEvent, "Cleaning Up", ContextPopup.TOMO_GUIDE);
-  }
-
-  /**
-   * Update the dialog with the current advanced state
-   */
-  private void updateAdvanced() {
-    applicationManager.packMainWindow();
-  }
-  
-  //
-  //  Action function overides for buttons
-  //
-  public void buttonCancelAction(ActionEvent event) {
-    super.buttonCancelAction(event);
-    applicationManager.doneCleanUp();
-  }
-
-  public void buttonPostponeAction(ActionEvent event) {
-    super.buttonPostponeAction(event);
-    applicationManager.doneCleanUp();
-  }
-
-  public void buttonExecuteAction(ActionEvent event) {
-    super.buttonExecuteAction(event);
-    applicationManager.doneCleanUp();
-  }
-
 }
-/**
- * <p> $Log$ </p>
-*/ 
