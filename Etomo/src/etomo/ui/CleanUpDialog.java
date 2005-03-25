799374116357f0b960d9e704c5675de78e079ca6
sueh <sueh@localhost>
2005-03-25 21:28:34 +0000

    bug# 621 backported from 3.6

--- Etomo/src/etomo/ui/CleanUpDialog.java
+++ Etomo/src/etomo/ui/CleanUpDialog.java
@@ -1,4 +1,15 @@
 package etomo.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+
+import etomo.ApplicationManager;
+import etomo.type.AxisID;
+import etomo.type.DialogType;
+
 /**
 * <p>Description: </p>
 * 
@@ -11,9 +22,71 @@ package etomo.ui;
 * @author $Author$
 * 
 * @version $Revision$
-* 
-* <p> $Log$ </p>
 */
-public class CleanUpDialog {
+public class CleanUpDialog extends ProcessDialog implements ContextMenu {
   public static  final String  rcsid =  "$Id$";
+  
+  private CleanupPanel cleanupPanel;
+  
+  public CleanUpDialog(ApplicationManager appMgr) {
+    super(appMgr, AxisID.ONLY, DialogType.CLEAN_UP);
+    rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.Y_AXIS));
+    cleanupPanel = new CleanupPanel(applicationManager);
+    rootPanel.add(cleanupPanel.getContainer());
+    rootPanel.add(Box.createVerticalGlue());
+    rootPanel.add(Box.createRigidArea(FixedDim.x0_y10));
+    rootPanel.add(pnlExitButtons);
+    rootPanel.add(Box.createRigidArea(FixedDim.x0_y10));
+    
+    btnAdvanced.setVisible(false);
+    btnExecute.setText("Done");
+
+
+    //  Mouse adapter for context menu
+    GenericMouseAdapter mouseAdapter = new GenericMouseAdapter(this);
+    rootPanel.addMouseListener(mouseAdapter);
+
+    // Set the default advanced dialog state
+    updateAdvanced();
+  }
+
+  /**
+   * Right mouse button context menu
+   */
+  public void popUpContextMenu(MouseEvent mouseEvent) {
+    new ContextPopup(rootPanel, mouseEvent, "Cleaning Up", ContextPopup.TOMO_GUIDE);
+  }
+
+  /**
+   * Update the dialog with the current advanced state
+   */
+  private void updateAdvanced() {
+    applicationManager.packMainWindow();
+  }
+  
+  //
+  //  Action function overides for buttons
+  //
+  public void buttonCancelAction(ActionEvent event) {
+    super.buttonCancelAction(event);
+    applicationManager.doneCleanUp();
+  }
+
+  public void buttonPostponeAction(ActionEvent event) {
+    super.buttonPostponeAction(event);
+    applicationManager.doneCleanUp();
+  }
+
+  public void buttonExecuteAction(ActionEvent event) {
+    super.buttonExecuteAction(event);
+    applicationManager.doneCleanUp();
+  }
+
 }
+/**
+* <p> $Log$
+* <p> Revision 1.1.2.1  2005/03/24 19:34:19  sueh
+* <p> bug# 621 Moved the clean up panel in post processing to a separate
+* <p> dialog.
+* <p> </p>
+*/
