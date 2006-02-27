ca18cebd63d1e44f1c5017f4fc3b3163679c8e50
sueh <sueh@localhost>
2006-02-27 20:06:07 +0000

    bug# 830 SetupComScripts():  sending all error messages and warnings to popup dialogs.  Since errors are handled, don't throw an exception.

--- Etomo/src/etomo/process/ProcessManager.java
+++ Etomo/src/etomo/process/ProcessManager.java
@@ -20,6 +20,10 @@
  * 
  * <p>
  * $Log$
+ * Revision 3.88.2.1  2006/02/22 19:07:57  sueh
+ * bug# 817 ModelToPatch():  don't try to move patch.out to patch.out~ if
+ * patch.out doesn't exist.
+ *
  * Revision 3.88  2005/12/09 20:28:09  sueh
  * bug# 776 Added non-abstract super.postProcessing to handle
  * tomosnapshot
@@ -752,7 +756,6 @@ import etomo.comscript.ConstSqueezevolParam;
 import etomo.comscript.ConstTiltalignParam;
 import etomo.comscript.ConstTiltxcorrParam;
 import etomo.comscript.CopyTomoComs;
-import etomo.comscript.BadComScriptException;
 import etomo.comscript.ExtractmagradParam;
 import etomo.comscript.ExtractpiecesParam;
 import etomo.comscript.ExtracttiltsParam;
@@ -794,8 +797,7 @@ public class ProcessManager extends BaseProcessManager {
    *          a read-only MetaData object containing the information to run the
    *          copytomocoms script
    */
-  public void setupComScripts(ConstMetaData metaData, AxisID axisID)
-      throws BadComScriptException, IOException {
+  public boolean setupComScripts(ConstMetaData metaData, AxisID axisID) {
     CopyTomoComs copyTomoComs = new CopyTomoComs(appManager);
 
     if (EtomoDirector.getInstance().isDebug()) {
@@ -804,37 +806,24 @@ public class ProcessManager extends BaseProcessManager {
     }
     appManager.saveIntermediateParamFile(axisID);
     int exitValue = copyTomoComs.run();
-
-    if (exitValue != 0) {
-      System.err.println("Exit value: " + String.valueOf(exitValue));
-
-      //  Compile the exception message from the stderr stream
-      String[] stdError = copyTomoComs.getStdError();
-      if (stdError == null || stdError.length < 1) {
-        stdError = new String[1];
-        stdError[0] = "Get David to add some std error reporting to copytomocoms";
-      }
-      StringBuffer buffer = new StringBuffer();
-      buffer.append("Copytomocoms Error\n");
-      buffer.append("Standard error output:\n");
-      for (int i = 0; i < stdError.length; i++) {
-        buffer.append(stdError[i]);
-        buffer.append("\n");
-      }
-
-      throw (new BadComScriptException(buffer.toString()));
-    }
-    else {
-      ProcessMessages messages = copyTomoComs.getProcessMessages();
+    //process messages
+    ProcessMessages messages = copyTomoComs.getProcessMessages();
+    if (messages.isError()) {
+      StringBuffer errorMessage = new StringBuffer("Error running Copytomocoms");
       for (int i = 0; i < messages.errorListSize(); i++) {
-        UIHarness.INSTANCE.openMessageDialog(messages.getError(i),
-            "Copytomocoms Error", axisID);
+        errorMessage.append("\n" + messages.getError(i));
       }
-      for (int i = 0; i < messages.warningListSize(); i++) {
-        UIHarness.INSTANCE.openMessageDialog(messages.getWarning(i),
-            "Copytomocoms Warning", axisID);
-      }
+      UIHarness.INSTANCE.openMessageDialog(errorMessage.toString(),
+          "Copytomocoms Error", axisID);
     }
+    for (int i = 0; i < messages.warningListSize(); i++) {
+      UIHarness.INSTANCE.openMessageDialog(messages.getWarning(i),
+          "Copytomocoms Warning", axisID);
+    }
+    if (exitValue != 0) {
+      return false;
+    }
+    return true;
   }
 
   /**
