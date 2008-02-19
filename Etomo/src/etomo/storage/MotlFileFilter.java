edbe85638130e0a0d7a7b088bd34a7e4f71c64ca
sueh <sueh@localhost>
2008-02-19 00:38:07 +0000

    bug# 1079 Added .EM and .csv file extensions.

--- Etomo/src/etomo/storage/MotlFileFilter.java
+++ Etomo/src/etomo/storage/MotlFileFilter.java
@@ -3,36 +3,41 @@ package etomo.storage;
 import java.io.File;
 
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright 2006</p>
-*
-* <p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-* 
-* <p> $Log$
-* <p> Revision 1.1  2007/03/01 01:14:47  sueh
-* <p> bug# 964 File filter for MOTL files (PEET interface).
-* <p> </p>
-*/
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2006</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$
+ * <p> Revision 1.2  2007/06/05 18:10:04  sueh
+ * <p> bug# 1011 Accepting any file that ends in .em.
+ * <p>
+ * <p> Revision 1.1  2007/03/01 01:14:47  sueh
+ * <p> bug# 964 File filter for MOTL files (PEET interface).
+ * <p> </p>
+ */
 public final class MotlFileFilter extends javax.swing.filechooser.FileFilter
-implements java.io.FileFilter {
-  public static  final String  rcsid =  "$Id$";
-  
+    implements java.io.FileFilter {
+  public static final String rcsid = "$Id$";
+
   public boolean accept(File f) {
     //  If this is a file test its extension, all others should return true
-    if (f.isFile() && !f.getAbsolutePath().endsWith(".em")) {
+    if (f.isFile() && !f.getAbsolutePath().endsWith(".em")
+        && !f.getAbsolutePath().endsWith(".EM")
+        && !f.getAbsolutePath().endsWith(".csv")) {
       return false;
     }
     return true;
   }
 
   public String getDescription() {
-    return "MOTL file (.em)";
+    return "MOTL file (.em, .csv)";
   }
 }
