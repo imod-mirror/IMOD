968215d3fe722466053a474a7315ca8c9eaa149c
sueh <sueh@localhost>
2011-09-19 18:39:09 -0600

    Bug# 1406 Added enableSelectionField.

---
+++ Etomo/src/etomo/ui/swing/ProcessDisplay.java
@@ -0,0 +1,27 @@
+package etomo.ui.swing;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2008</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$
+* <p> Revision 1.1  2010/11/13 16:07:34  sueh
+* <p> bug# 1417 Renamed etomo.ui to etomo.ui.swing.
+* <p>
+* <p> Revision 1.1  2009/06/11 17:01:18  sueh
+* <p> bug# 1221 Generic interface for panels which contain process
+* <p> parameters.  Used with next process functionality.
+* <p> </p>
+*/
+public interface ProcessDisplay {
+  public static final String rcsid = "$Id$";
+}
