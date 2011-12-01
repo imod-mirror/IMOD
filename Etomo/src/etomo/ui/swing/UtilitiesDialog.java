2a74a9b3bee1b8ad12fefc6041e644ebe224dc09
John Heumann <john.heumann@colorado.edu>
2011-12-01 10:39:30 -0700

    flgAbsValue enum was reversed

---
+++ Etomo/src/etomo/ui/swing/UtilitiesDialog.java
@@ -0,0 +1,26 @@
+package etomo.ui.swing;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2010</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$
+* <p> Revision 1.1  2010/11/13 16:07:34  sueh
+* <p> bug# 1417 Renamed etomo.ui to etomo.ui.swing.
+* <p>
+* <p> Revision 3.1  2010/02/17 05:03:12  sueh
+* <p> bug# 1301 Using manager instead of manager key for popping up messages.
+* <p> </p>
+*/
+public final class UtilitiesDialog {
+  public static final String rcsid = "$Id$";
+}
