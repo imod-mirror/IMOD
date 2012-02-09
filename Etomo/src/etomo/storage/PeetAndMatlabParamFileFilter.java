a85ee776b805c4c8bb56b49617b13e83f83d7330
mast <mast@localhost>
2012-02-09 15:13:49 -0700

    Removed spaces properly this time

---
+++ Etomo/src/etomo/storage/PeetAndMatlabParamFileFilter.java
@@ -0,0 +1,44 @@
+package etomo.storage;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+import etomo.util.DatasetFiles;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2006</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$
+ * <p> Revision 1.1  2007/06/04 23:06:34  sueh
+ * <p> bug# 1005 Accepts .prm and .epe files.
+ * <p> </p>
+ */
+public final class PeetAndMatlabParamFileFilter extends FileFilter {
+  public static final String rcsid = "$Id$";
+
+  public boolean accept(File f) {
+    //  If this is a file test its extension, all others should return true
+    if (f.isFile() && !f.getAbsolutePath().endsWith(DatasetFiles.MATLAB_PARAM_FILE_EXT)
+        && !f.getAbsolutePath().endsWith(DatasetFiles.PEET_DATA_FILE_EXT)) {
+      return false;
+    }
+    return true;
+  }
+
+  public String getDescription() {
+    return "MATLAB param file (" + DatasetFiles.MATLAB_PARAM_FILE_EXT + ", "
+        + DatasetFiles.PEET_DATA_FILE_EXT + ")";
+  }
+
+}
