7fbb6af637f5d5479c463754b735a9f6d649e760
sueh <sueh@localhost>
2012-09-07 15:15:07 -0600

    Automated merge with file:///home/sueh/workspace/Completed

--- Etomo/src/etomo/logic/VersionControl.java
+++ Etomo/src/etomo/logic/VersionControl.java
@@ -34,7 +34,7 @@ import etomo.util.EnvironmentVariable;
 public final class VersionControl {
   public final String rcsid = "$$Id$$";
 
-  public static final String TIME_STAMP = "8/28/2012 16:10";
+  public static final String TIME_STAMP = "9/6/2012 21:17";
 
   private static final String DEPENDANT_IMOD_VERSION = "4.4.4";
   private static final String DEPENDANT_PEET_VERSION = "1.8.0";
