3eb76c56318572927a7c21c1277d966c8764375b
sueh <sueh@localhost>
2014-05-20 17:37:38 -0600

    Bug# 1790 Added isDifferentFromChecking.

--- Etomo/src/etomo/ui/swing/CheckBoxCell.java
+++ Etomo/src/etomo/ui/swing/CheckBoxCell.java
@@ -28,10 +28,11 @@ final class CheckBoxCell extends InputCell implements ToggleCell {
   public static final String rcsid = "$Id$";
 
   private JCheckBox checkBox = new JCheckBox();
-  //label: from JCheckBox.getText().  Updated in setLabel().  Always up to date
-  //because it is a read only field in JCheckBox.
+  // label: from JCheckBox.getText(). Updated in setLabel(). Always up to date
+  // because it is a read only field in JCheckBox.
   private String unformattedLabel = "";
   private boolean enabled = true;
+  private EtomoBoolean2 checkpointValue = null;
 
   CheckBoxCell() {
     super();
@@ -48,6 +49,18 @@ final class CheckBoxCell extends InputCell implements ToggleCell {
 
   UITestFieldType getFieldType() {
     return UITestFieldType.CHECK_BOX;
+  }
+
+  /**
+   * 
+   * @param alwaysCheck - check for difference even when the field is disables or invisible
+   * @return
+   */
+  boolean isDifferentFromCheckpoint(final boolean alwaysCheck) {
+    if (!alwaysCheck && (!isEnabled() || !checkBox.isVisible())) {
+      return false;
+    }
+    return checkpointValue == null || !checkpointValue.equals(isSelected());
   }
 
   public void setEnabled(final boolean enabled) {
@@ -72,7 +85,7 @@ final class CheckBoxCell extends InputCell implements ToggleCell {
   void setValue(final String value) {
     checkBox.setSelected(new EtomoBoolean2().set(value).is());
   }
-  
+
   public boolean isEnabled() {
     return checkBox.isEnabled();
   }
