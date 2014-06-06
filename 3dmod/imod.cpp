b16b6ff5ef4378e5776414b6b0f1c93bed8d8ca1
sueh <sueh@localhost>
2014-06-06 08:00:55 -0600

    Automated merge with file:///home/sueh/workspace/Completed

--- 3dmod/imod.cpp
+++ 3dmod/imod.cpp
@@ -806,8 +806,18 @@ int main( int argc, char *argv[])
 
     } else {
 
-      /* It is a single image file - build list with this image */
+      // It is a single image file - build list with this image
       ivwMultipleFiles(&vi, &Imod_imagefile, 0, 0, anyHavePieceList);
+
+      // Then if there is no piece list entered or in image, look for a pl file
+      if (!frames && !plistfname && !anyHavePieceList) {
+        QString plname =  info.path() + QDir::separator() + info.completeBaseName() +
+          ".pl";
+        if (QFile::exists(plname)) {
+          plFileNames << plname;
+          plistfname = strdup(LATIN1(plname));
+        }
+      }
     }
   } else {
 
