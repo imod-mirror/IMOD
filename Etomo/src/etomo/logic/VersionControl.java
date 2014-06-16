d1397105dbd3f2cb85ddc4b121b57d76f18b1a35
sueh <sueh@localhost>
2014-06-16 11:19:57 -0600

    Automated merge with file:///home/hg/IMOD

--- Etomo/src/etomo/logic/VersionControl.java
+++ Etomo/src/etomo/logic/VersionControl.java
@@ -37,7 +37,7 @@ public final class VersionControl {
   private static final String DEPENDANT_IMOD_VERSION = "4.4.4";
   private static final String DEPENDANT_PEET_VERSION = "1.8.0";
   //Updated by a script
-  public static final String TIME_STAMP = "6/11/2014 13:44";
+  public static final String TIME_STAMP = "6/13/2014 15:32";
 
   /**
 
