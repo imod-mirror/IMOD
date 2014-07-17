6283a95938f2561ea2f40a2343300ad1a09e6cde
sueh <sueh@localhost>
2014-07-17 10:08:20 -0600

    Bug# 1790 Removed unused import.

--- Etomo/src/etomo/ui/swing/RadioTextField.java
+++ Etomo/src/etomo/ui/swing/RadioTextField.java
@@ -4,7 +4,6 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
