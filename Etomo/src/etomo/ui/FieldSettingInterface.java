16572836044e5236f8b971162fdc5b62dd281dbb
sueh <sueh@localhost>
2014-11-02 12:21:06 -0700

    Bug# 1790 Added isBoolean, isText, and isSet.

--- Etomo/src/etomo/ui/FieldSettingInterface.java
+++ Etomo/src/etomo/ui/FieldSettingInterface.java
@@ -1,24 +1,25 @@
 package etomo.ui;
 
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright 2014</p>
-*
-* <p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-* 
-* <p> $Log$ </p>
-*/
+ * <p>Description: </p>
+ * <p/>
+ * <p>Copyright: Copyright 2014</p>
+ * <p/>
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ *
+ * @author $Author$
+ * @version $Revision$  $Date: $
+ */
 public interface FieldSettingInterface {
-  public static final String rcsid = "$Id:$";
-
   public BooleanFieldSetting getBooleanSetting();
 
   public TextFieldSetting getTextSetting();
+
+  public boolean isBoolean();
+
+  public boolean isText();
+
+  public boolean isSet();
 }
