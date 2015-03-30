15456c4df16b99c1c1ceb3e6a18c44ed53bcd3e8
sueh <sueh@localhost>
2015-03-30 12:52:18 -0600

    Bug# 1865 Added DUALVOLMATCH.  Replaced SOLVEMATCH_INDEX with SOLVEMATCH_DUALVOLMATCH_INDEX.

--- Etomo/src/etomo/type/CombineProcessType.java
+++ Etomo/src/etomo/type/CombineProcessType.java
@@ -3,32 +3,29 @@ package etomo.type;
 /**
  * <p>Description: </p>
  * 
- * <p>Copyright: Copyright (c) 2006</p>
+ * <p>Copyright: Copyright 2006 - 2015 by the Regents of the University of Colorado</p>
+ * <p/>
+ * <p>Organization: Dept. of MCD Biology, University of Colorado</p>
  *
- * <p>Organization:
- * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
- * University of Colorado</p>
- * 
- * @author $Author$
- * 
- * @version $Revision$
+ * @version $Id$
  */
 public final class CombineProcessType {
-  public static final String rcsid = "$Id$";
-
-  public static final int SOLVEMATCH_INDEX = 0;
+  public static final int SOLVEMATCH_DUALVOLMATCH_INDEX = 0;
   public static final int MATCHVOL1_INDEX = 1;
   public static final int PATCHCORR_INDEX = 2;
   public static final int MATCHORWARP_INDEX = 3;
   public static final int VOLCOMBINE_INDEX = 4;
-  
+
   public static final int TOTAL = VOLCOMBINE_INDEX + 1;
 
   private final int index;
-  
+
   public static CombineProcessType getInstance(String processName) {
     if (ProcessName.SOLVEMATCH.equals(processName)) {
       return SOLVEMATCH;
+    }
+    if (ProcessName.DUALVOLMATCH.equals(processName)) {
+      return DUALVOLMATCH;
     }
     if (ProcessName.MATCHVOL1.equals(processName)) {
       return MATCHVOL1;
@@ -44,10 +41,17 @@ public final class CombineProcessType {
     }
     return null;
   }
-  
-  public static CombineProcessType getInstance(int processIndex) {
-    if (processIndex == SOLVEMATCH_INDEX) {
-      return SOLVEMATCH;
+
+  public static CombineProcessType getInstance(final int processIndex,
+   final boolean initialVolumeMatching) {
+    System.out.println("B:initialVolumeMatching:"+initialVolumeMatching);
+    if (processIndex == SOLVEMATCH_DUALVOLMATCH_INDEX) {
+      if (!initialVolumeMatching) {
+        return SOLVEMATCH;
+      }
+      else {
+        return DUALVOLMATCH;
+      }
     }
     if (processIndex == MATCHVOL1_INDEX) {
       return MATCHVOL1;
@@ -69,16 +73,18 @@ public final class CombineProcessType {
   }
 
   public static final CombineProcessType SOLVEMATCH = new CombineProcessType(
-      SOLVEMATCH_INDEX);
+    SOLVEMATCH_DUALVOLMATCH_INDEX);
+  public static final CombineProcessType DUALVOLMATCH = new CombineProcessType(
+    SOLVEMATCH_DUALVOLMATCH_INDEX);
   public static final CombineProcessType MATCHVOL1 = new CombineProcessType(
-      MATCHVOL1_INDEX);
+    MATCHVOL1_INDEX);
   public static final CombineProcessType PATCHCORR = new CombineProcessType(
-      PATCHCORR_INDEX);
+    PATCHCORR_INDEX);
   public static final CombineProcessType MATCHORWARP = new CombineProcessType(
-      MATCHORWARP_INDEX);
+    MATCHORWARP_INDEX);
   public static final CombineProcessType VOLCOMBINE = new CombineProcessType(
-      VOLCOMBINE_INDEX);
-  
+    VOLCOMBINE_INDEX);
+
   public int getIndex() {
     return index;
   }
@@ -86,6 +92,9 @@ public final class CombineProcessType {
   public String toString() {
     if (this == SOLVEMATCH) {
       return ProcessName.SOLVEMATCH.toString();
+    }
+    if (this == DUALVOLMATCH) {
+      return ProcessName.DUALVOLMATCH.toString();
     }
     if (this == MATCHVOL1) {
       return ProcessName.MATCHVOL1.toString();
