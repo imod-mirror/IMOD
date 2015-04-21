4bc3283f5ad3d1aaa4b1aca0ed7758fe13526407
sueh <sueh@localhost>
2015-04-21 13:47:45 -0600

    Bug# 1874 Added isInvertYLimits.

---
+++ Etomo/src/etomo/logic/CombineTool.java
@@ -0,0 +1,23 @@
+package etomo.logic;
+
+import java.io.File;
+
+import etomo.BaseManager;
+import etomo.EtomoDirector;
+import etomo.type.FileType;
+
+/**
+ * <p>Description: Logic for combine. </p>
+ * <p/>
+ * <p>Copyright: Copyright 2015 by the Regents of the University of Colorado</p>
+ * <p/>
+ * <p>Organization: Dept. of MCD Biology, University of Colorado</p>
+ *
+ * @version $Id$
+ */
+public final class CombineTool {
+  public boolean isInvertYLimits(final BaseManager manager) {
+   //return FileType.ORIG_COMS_DIR.exists(manager);
+    return false;
+  }
+}
