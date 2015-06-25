512793e1d6e9157b1dc48943ae30e5093b937fbe
sueh <sueh@localhost>
2015-06-25 15:50:16 -0600

    Bug# 1871

---
+++ Etomo/src/etomo/type/Status.java
@@ -0,0 +1,12 @@
+package etomo.type;
+
+/**
+* <p>Description: Works with StatusChangeListener.</p>
+* 
+* <p>Copyright: Copyright 2015 by the Regents of the University of Colorado</p>
+* <p/>
+* <p>Organization: Dept. of MCD Biology, University of Colorado</p>
+*
+* @version $Id$
+*/
+public interface Status {}
