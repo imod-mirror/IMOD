b13ca26f12af2a90516dd3bdb077a23a433e9ca1
sueh <sueh@localhost>
2015-07-02 01:28:02 -0600

    Bug# 1871 Added processManager, processMonitor.  Added parse, runMsgComScriptDone.

--- Etomo/src/etomo/process/OutfileComScriptProcess.java
+++ Etomo/src/etomo/process/OutfileComScriptProcess.java
@@ -1,14 +1,45 @@
 package etomo.process;
 
+import java.io.FileNotFoundException;
+
 import etomo.BaseManager;
 import etomo.comscript.Command;
+import etomo.storage.LogFile;
 import etomo.type.AxisID;
 import etomo.type.FileType;
 
 public final class OutfileComScriptProcess extends ComScriptProcess {
+  private final BaseProcessManager processManager;
+  private final DetachedProcessMonitor processMonitor;
+
   public OutfileComScriptProcess(final BaseManager manager, final String comScript,
     final BaseProcessManager processManager, final AxisID axisID,
-    final ProcessMonitor processMonitor, final Command command, final FileType fileType) {
+    final DetachedProcessMonitor processMonitor, final Command command,
+    final FileType fileType) {
     super(manager, comScript, processManager, axisID, processMonitor, command, fileType);
+    this.processManager = processManager;
+    this.processMonitor = processMonitor;
   }
+
+  /**
+   * Need this so the right msgComScriptDone function can be called.
+   */
+  void runMsgComScriptDone(final int exitValue) {
+    try {
+      // Wait for the monitor to complete.
+      while (processMonitor.isProcessRunning()) {
+        Thread.sleep(100);
+      }
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    processManager.msgComScriptDone(this, exitValue, getNonBlocking());
+  }
+
+  /**
+   * Not not parse the log file for warnings.
+   */
+  void parse(String name, boolean mustExist) throws LogFile.LockException,
+    FileNotFoundException {}
 }
