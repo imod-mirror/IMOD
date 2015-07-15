525feac7887a63acff847c2d198c77e6bb8c0753
sueh <sueh@localhost>
2015-07-15 09:18:17 -0600

    Bug# 1871 dataset-level status

---
+++ Etomo/src/etomo/type/BatchRunTomoDatasetStatus.java
@@ -0,0 +1,14 @@
+package etomo.type;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2015 by the Regents of the University of Colorado</p>
+* <p/>
+* <p>Organization: Dept. of MCD Biology, University of Colorado</p>
+*
+* @version $Id$
+*/
+public final class BatchRunTomoDatasetStatus implements Status {
+
+}
