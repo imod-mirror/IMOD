b181e42721fb22b8d203d9f8570cd64b8580e60a
sueh <sueh@localhost>
2015-07-15 12:54:53 -0600

    Bug# 1871 Limiting status by type rather then using a template.  Added identifier.  Added getStatus, getIdentifier.

--- Etomo/src/etomo/type/StatusChangeEvent.java
+++ Etomo/src/etomo/type/StatusChangeEvent.java
@@ -1,9 +1,28 @@
 package etomo.type;
 
-public final class StatusChangeEvent<S> {
-  private final S status;
+/**
+ * <p>Description: Bundles status with an identifier.</p>
+ * <p/>
+ * <p>Copyright: Copyright 2014 - 2015 by the Regents of the University of Colorado</p>
+ * <p/>
+ * <p>Organization: Dept. of MCD Biology, University of Colorado</p>
+ *
+ * @version $Id$
+ */
+public final class StatusChangeEvent {
+  private final Status status;
+  private final String identifier;
 
- public StatusChangeEvent(final S status) {
+  public StatusChangeEvent(final Status status, final String identifier) {
     this.status = status;
+    this.identifier = identifier;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public String getIdentifier() {
+    return identifier;
   }
 }
