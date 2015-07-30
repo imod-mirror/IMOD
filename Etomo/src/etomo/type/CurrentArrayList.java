98f0a331c18ab10a281ddd70043d7cb929d44a95
sueh <sueh@localhost>
2015-07-29 23:12:22 -0600

    Bug# 1871 Renamed currentIndex to index, and stateInt to total.  Removed clear.  Renamed getCurrentIndex to getIndex, and getStateInt to getTotal.  Added incrementTotal, next, previous.  Removed setCurrentIndex, setCurrentIndexToLastElement, and setStateInt.

--- Etomo/src/etomo/type/CurrentArrayList.java
+++ Etomo/src/etomo/type/CurrentArrayList.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 /**
 * <p>Description: An array list with some state information:  a current index and another
-* integer.</p>
+* integer (total).  The index points to the first element, unless it is incremented.  The
+* total variable defaults to zero.</p>
 * 
 * <p>Copyright: Copyright 2015 by the Regents of the University of Colorado</p>
 * <p/>
@@ -13,8 +14,10 @@ import java.util.ArrayList;
 * @version $Id$
 */
 public final class CurrentArrayList<E> extends ArrayList<E> {
-  private int currentIndex = -1;
-  private int stateInt = -1;
+  private int index = -1;
+  // May be set equal to the size of a subset of elemnts - or anything else. No controls
+  // on this variable - just part of the bundle.
+  private int total = 0;
 
   public CurrentArrayList() {
     super();
@@ -22,55 +25,59 @@ public final class CurrentArrayList<E> extends ArrayList<E> {
 
   public CurrentArrayList(CurrentArrayList<E> input) {
     super(input);
-    currentIndex = input.currentIndex;
-    stateInt = input.stateInt;
-  }
-
-  public void clear() {
-    super.clear();
+    index = input.index;
+    total = input.total;
   }
 
   /**
-   * Sets current index to the size of the list minus one.
+   * Gets the element that the index is pointing to.  Returns null if the array is
+   * empty, or the index isn't valid.
    */
-  public void setCurrentIndexToLastElement() {
-    if (isEmpty()) {
-      currentIndex = -1;
+  public E getCurrent() {
+    if (isEmpty() || index < 0 || index >= size()) {
+      return null;
     }
-    else {
-      // Treating null (possibly removed) elements as real elements.
-      currentIndex = size() - 1;
-    }
+    return get(index);
+  }
+
+  public void next() {
+    index++;
+  }
+
+  public void previous() {
+    index--;
+  }
+
+  public int getIndex() {
+    return index;
   }
 
   /**
-   * Sets an index - will fix if it is not legal
-   * @param input
+   * Copies the state of input, up to and including the current element.
+   * After this function is called, getCurrent will return the last element.  Does not
+   * deep copy the elements.
+   * @param input - from instance.
    */
-  public void setCurrentIndex(final int input) {
-    if (isEmpty()) {
-      currentIndex = -1;
+  public void copyToCurrent(final CurrentArrayList<E> input) {
+    if (input == null) {
+      clear();
+      index = -1;
+      total = 0;
+      return;
     }
-    else if (input < 0) {
-      currentIndex = 0;
-    }
-    else if (input >= size()) {
-      currentIndex = size() - 1;
-    }
-    else {
-      currentIndex = input;
+    index = input.index;
+    total = input.total;
+    int size = Math.min(input.index + 1, input.size());
+    for (int i = 0; i < size; i++) {
+      add(input.get(i));
     }
   }
 
-  public void setStateInt(final int input) {
-    stateInt = input;
+  public void incrementTotal() {
+    total++;
   }
 
-  public int getCurrentIndex() {
-    return currentIndex;
-  }
-
-  public int getStateInt() {
-    return stateInt;
+  public int getTotal() {
+    return total;
   }
 }
