45918062d1cf0be1bec4041f6ba5ba120652e07a
sueh <sueh@localhost>
2015-08-01 21:08:20 -0600

    Bug# 1871 Added DEFAULT.  Added getInstance.

--- Etomo/src/etomo/type/BatchRunTomoStatus.java
+++ Etomo/src/etomo/type/BatchRunTomoStatus.java
@@ -18,10 +18,31 @@ public final class BatchRunTomoStatus implements Status {
     "Killed/Paused");
   public static final BatchRunTomoStatus STOPPED = new BatchRunTomoStatus("Stopped");
 
+  public static final BatchRunTomoStatus DEFAULT = OPEN;
+
   private final String text;
 
   private BatchRunTomoStatus(final String text) {
     this.text = text;
+  }
+
+  public static BatchRunTomoStatus getInstance(final String text) {
+    if (text == null) {
+      return DEFAULT;
+    }
+    if (OPEN.text.equals(text)) {
+      return OPEN;
+    }
+    if (RUNNING.text.equals(text)) {
+      return RUNNING;
+    }
+    if (KILLED_PAUSED.text.equals(text)) {
+      return KILLED_PAUSED;
+    }
+    if (STOPPED.text.equals(text)) {
+      return STOPPED;
+    }
+    return DEFAULT;
   }
 
   public String getText() {
