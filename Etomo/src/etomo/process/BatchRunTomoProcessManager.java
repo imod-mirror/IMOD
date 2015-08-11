6e57598d43e76278040c198438533f6d617c0f77
sueh <sueh@localhost>
2015-08-11 15:00:28 -0600

    Bug# 1871 In batchruntomo added ProcessMessages parameter.  Added haltMonitorThread, reconnectBatchruntomo.

--- Etomo/src/etomo/process/BatchRunTomoProcessManager.java
+++ Etomo/src/etomo/process/BatchRunTomoProcessManager.java
@@ -2,9 +2,12 @@ package etomo.process;
 
 import etomo.BatchRunTomoManager;
 import etomo.comscript.BatchruntomoParam;
+import etomo.storage.LogFile;
 import etomo.type.AxisID;
 import etomo.type.CurrentArrayList;
 import etomo.type.FileType;
+import etomo.type.ProcessName;
+import etomo.ui.swing.UIHarness;
 
 /**
 * <p>Description: </p>
@@ -30,17 +33,54 @@ public final class BatchRunTomoProcessManager extends BaseProcessManager {
    * @param runKeys - array of keys to the rows involved with this run
    */
   public String batchruntomo(final BatchruntomoParam param,
-    final CurrentArrayList<String> runKeys) throws SystemProcessException {
+    final CurrentArrayList<String> runKeys, final ProcessMessages messages)
+    throws SystemProcessException {
     if (param == null) {
       return null;
     }
     String command = FileType.BATCH_RUN_TOMO_COMSCRIPT.getFileName(manager, AXID_ID);
+    ProcessData managedProcessData =
+      ProcessData.getManagedInstance(AXID_ID, manager, ProcessName.BATCHRUNTOMO);
     BatchRunTomoProcessMonitor monitor =
-      new BatchRunTomoProcessMonitor(manager, AXID_ID, true, runKeys);
+      BatchRunTomoProcessMonitor.getInstance(manager, AXID_ID, runKeys,
+        managedProcessData, messages);
     // Start the com script in the background
     ComScriptProcess comScriptProcess =
       startOutfileComScript(command, monitor, AXID_ID, param,
-        FileType.BATCH_RUN_TOMO_COMSCRIPT);
+        FileType.BATCH_RUN_TOMO_COMSCRIPT, ProcessName.BATCHRUNTOMO, true,
+        managedProcessData);
     return comScriptProcess.getName();
+  }
+
+  public  boolean reconnectBatchruntomo(final ProcessData processData,
+    final ProcessMessages messages) {
+    BatchRunTomoProcessMonitor monitor =
+      BatchRunTomoProcessMonitor.getReconnectInstance(manager, AXID_ID, processData,
+        messages);
+    boolean ret;
+    try {
+      ReconnectProcess process =
+        new LoggedReconnectProcess(manager, this, monitor,
+          axisProcessData.getSavedProcessData(AXID_ID), AXID_ID,
+          monitor.getLogFileName(), ProcessOutputStrings.BRT_SUCCESS_TAG, null, null,
+          false, true);
+      monitor.setProcess(process);
+      Thread thread = new Thread(process);
+      thread.start();
+      axisProcessData.mapAxisThread(process, AXID_ID);
+      axisProcessData.mapAxisProcessMonitor(null, monitor, AXID_ID);
+    }
+    catch (LogFile.LockException e) {
+      e.printStackTrace();
+      UIHarness.INSTANCE.openMessageDialog(manager,
+        "Unable to reconnect to processchunks.\n" + e.getMessage(), "Reconnect Failure",
+        AXID_ID);
+      return false;
+    }
+    return true;
+  }
+  
+  public void haltMonitorThread() {
+    axisProcessData.haltMonitorThread(AXID_ID);
   }
 }
